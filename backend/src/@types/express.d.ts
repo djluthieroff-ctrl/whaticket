1154df57506ae946115e832701b80af284867129
djluthieroff-ctrl <djluthieroff@gmail.com>
2025-11-25 11:29:10 -0300

    Fix TypeScript Multer types error

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -1,5 +1,23 @@
-declare namespace Express {
-  export interface Request {
-    user: { id: string; profile: string; companyId: number };
+import { Request as ExpressRequest } from "express";
+
+declare global {
+  namespace Express {
+    export interface Request {
+      user: { id: string; profile: string; companyId: number };
+    }
+
+    namespace Multer {
+      export interface File {
+        fieldname: string;
+        originalname: string;
+        encoding: string;
+        mimetype: string;
+        size: number;
+        destination: string;
+        filename: string;
+        path: string;
+        buffer: Buffer;
+      }
+    }
   }
 }
